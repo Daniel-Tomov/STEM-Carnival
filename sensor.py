b047d2b4ff80ed91875e9db25ee1fb3c59b3b1bc
Daniel-Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2023-09-23 15:22:19 -0400

    feat: provide example sensor code

--- sensor.py
+++ sensor.py
@@ -1,4 +1,7 @@
 import client
 
 
-client.client_send("")
+# provide the name of the sensor and the data from the sensor
+# sensor_name is not stored in the secrets because there may
+# be more than one sensor tied to one client
+client.client_send("height", "100")
